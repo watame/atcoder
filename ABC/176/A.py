6742e9bbe1d701e758aaca8aaddd60a68afe51c1
watame <pandora.border@gmail.com>
2022-08-14 22:29:16 +0900

    :+1: 解答更新

--- ABC/176/A.py
+++ ABC/176/A.py
@@ -1,7 +1,9 @@
 # A - Takoyaki
 goal_count, make_count, make_minutes = map(int, input().split(" "))
 
-if goal_count % make_count == 0:
-    print(goal_count // make_count * make_minutes)
-else:
-    print((goal_count // make_count + 1) * make_minutes)
+# 頻出テクニックを利用すると場合分けが不要
+# 割られる数に X−1 を足すことで、ちょうど割り切れる時以外は答えが 1 増える
+# 例: goal_count = 10, make_count = 3
+#   (10 + 3 - 1) / 3 -> 12 / 3 -> 4
+# https://atcoder.jp/contests/abc176/editorial/60
+print((goal_count + make_count - 1) // make_count * make_minutes)

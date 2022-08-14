970c236d29621cfbf2c3538509df0a0e159fe7c6
watame <pandora.border@gmail.com>
2022-08-14 22:59:59 +0900

    :+1: 解答更新

---
+++ typical90/A.py
@@ -0,0 +1,13 @@
+# 033 - Not Too Bright（★2）
+height, width = map(int, input().split(" "))
+
+if height < 2 or width < 2:
+    # 2マス以上でなければ全部ライトアップすればOK
+    print(max(height, width))
+else:
+    # 4マスの中に2つ以上ライトアップするのはNG
+    # -> 縦、横で各2マスに1つしか存在できない
+    #    ※3マス目が存在した場合にも配置が可能なので、小技を使って切り上げ計算する
+    height_light = (height + 2 - 1) // 2
+    width_light = (width + 2 - 1) // 2
+    print(height_light * width_light)
